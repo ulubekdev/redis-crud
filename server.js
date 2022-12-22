ab9fa663f3fcb589a9bf5092c60b5d694c24c563
Mike Devy <ulugbekyolchimurodov@gmail.com>
2022-12-22 21:08:21 +0500

    Routed users

--- server.js
+++ server.js
@@ -1,2 +1,29 @@
 import redis from 'redis';
 import express from 'express';
+
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.status(200).send("Working");
+});
+
+app.get('/users', (req, res) => {
+    
+});
+
+app.post('/register', (req, res) => {
+
+});
+
+app.post('/login', (req, res) => {
+    
+});
+
+app.delete('/users/:user_id', (req, res) => {
+    
+});
+
+app.listen(PORT, () => {
+    console.log(`Server on http://localhost:${PORT}`);
+});
