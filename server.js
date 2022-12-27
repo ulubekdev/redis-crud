69d06ef9e912094adc39e9d5ad0cd05ded468246
Mike Devy <ulugbekyolchimurodov@gmail.com>
2022-12-28 01:58:21 +0500

    Finished

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import "./config.js";
 import express from "express";
 import { userRepository } from "./redis-om.js";
+
 const PORT = 5000 || process.env.PORT;
 const app = express();
-
 app.use(express.json());
 
 app.get("/", (res) => res.status(200).send("Working"));
@@ -30,16 +30,31 @@ app.post("/register", async (req, res) => {
 	}
 });
 
-app.post("/login", (req, res) => {});
+app.post("/login", async(req, res) => {
+    try {
+        let user = await userRepository.search().where('username').eq(req.body.username).return.first() && await userRepository.search().where('password').eq(req.body.password).return.first();
+        res.status(200).send(`Welcome ${user}`);
+    } catch (error) {
+		res.status(500).send(error.message);
+    }
+});
 
 app.put("/users/:id", async(req, res) => {
-    const user = await userRepository.fetch(req.params.id);
-    res.send(user);
+    try {
+        const user = await userRepository.fetch(req.params.id);
+        res.send(user);
+    } catch (error) {
+		res.status(500).send(error.message);
+    }
 });
 
 app.delete("/users/:id", async(req, res) => {
-    await userRepository.remove(req.params.id);
-    res.send({ entityId: req.params.id });
+    try {
+        await userRepository.remove(req.params.id);
+        res.send({ entityId: req.params.id });
+    } catch (error) {
+		res.status(500).send(error.message);
+    }
 });
 
 app.listen(PORT, () => {
